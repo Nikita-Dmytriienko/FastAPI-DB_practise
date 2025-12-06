a525e9cebc76d916e370a2e33ecf8fdc4acb06a9
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2025-12-06 18:49:07 +0200

    feat: implement /books GET endpoint to retrieve all books async

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import select
 
 app = FastAPI()
 
@@ -56,7 +57,10 @@ async def add_book(data: BookAddSchema, session: SessionDep):
 
 
 @app.get('/books')
-async def get_books():
-    ...
+async def get_books(session: SessionDep):
+    query = select(BookModel)
+    result = await session.execute(query)
+    return result.scalars().all()
+
 
 
